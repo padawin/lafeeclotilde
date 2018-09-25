148350e424b39cbc426f101b140e5708d5e5dde0
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2018-09-25 19:54:36 +0100

    Resize done when uploading the pictures

--- api/service/picture.py
+++ api/service/picture.py
@@ -1,4 +1,5 @@
 import errno
+import functools
 import json
 import os
 from enum import Enum, auto
@@ -41,6 +42,7 @@ class PictureService(picture.PictureService):
         file_path = self.save_file(file, relative_file_path)
         picture['exif'] = json.dumps(self._get_file_exif(file_path))
         PictureModel.update(picture, ['id_picture = %s', [picture_id]])
+        self.resize(relative_file_path)
         Model.commit()
         return PictureSaveResult.OK, None
 
@@ -77,6 +79,88 @@ class PictureService(picture.PictureService):
         # save the file
         file.save(file_path)
         return file_path
+
+    @staticmethod
+    def _image_transpose_exif(im):
+        """
+        Applies EXIF transformations to `im` (if present) and returns
+        the result as a new image.
+        Taken from [here](https://stackoverflow.com/a/30462851/1667018)
+        and adapted.
+        """
+        exif_orientation_tag = 0x0112  # contains an integer, 1 through 8
+        exif_transpose_sequences = [  # corresponding to the following
+            [],
+            [Image.FLIP_LEFT_RIGHT],
+            [Image.ROTATE_180],
+            [Image.FLIP_TOP_BOTTOM],
+            [Image.FLIP_LEFT_RIGHT, Image.ROTATE_90],
+            [Image.ROTATE_270],
+            [Image.FLIP_TOP_BOTTOM, Image.ROTATE_90],
+            [Image.ROTATE_90],
+        ]
+
+        try:
+            seq = exif_transpose_sequences[
+                im._getexif()[exif_orientation_tag] - 1
+            ]
+        except Exception:
+            return im
+        else:
+            if seq:
+                return functools.reduce(
+                    lambda im, op: im.transpose(op), seq, im
+                )
+            else:
+                return im
+
+    def _process_image_from_config(self, picture, config):
+        def new_img(size, im_to_paste=None):
+            im = Image.new(
+                "RGB",
+                (
+                    min(size[0], im_to_paste.size[0]),
+                    min(size[1], im_to_paste.size[1])
+                )
+            )
+            im.paste(
+                im_to_paste,
+                (
+                    min(0, (size[0] - im_to_paste.size[0]) // 2),
+                    min(0, (size[1] - im_to_paste.size[1]) // 2),
+                ),
+            )
+            return im
+        im = self._image_transpose_exif(
+            Image.open(picture)
+        )
+        size = (config["width"], config["height"])
+        scale_x = size[0] / im.size[0]
+        scale_y = size[1] / im.size[1]
+        if config["crop"]:
+            scale = max(scale_x, scale_y)
+        else:
+            scale = min(scale_x, scale_y)
+        size2 = tuple(int(round(value * scale)) for value in im.size)
+        im = im.resize(size2, Image.ANTIALIAS)
+        im = new_img(size, im)
+        return im
+
+    def resize(self, picture_path):
+        for config in self.config['PICTURES_RESIZE_FORMATS']:
+            im = self._process_image_from_config(
+                os.path.join(self.config['UPLOAD_DIRECTORY'], picture_path),
+                config
+            )
+            destination = "{directory}/{width}x{height}x{crop}/{path}".format(
+                directory=self.config['RESIZED_PICTURES_DIRECTORY'],
+                width=config['width'],
+                height=config['height'],
+                crop=int(config['crop']),
+                path=picture_path
+            )
+            self._create_dir(destination)
+            im.save(destination, "JPEG", quality=95, optimize=True)
 
     def get_all(self, offset, limit):
         pictures = PictureModel.loadAll(limit=limit, offset=offset)
